63aa825c21f27b866a2510d50d4944ed4a213286
Michel Metran <michelmetran@gmail.com>
2022-02-03 20:26:06 -0300

    AJuste

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ for line in open('requirements.txt'):
     if not li.startswith('#'):
         requirements.append(line.rstrip())
 
-VERSION = (1, 0, 5)  # (1, 0, 7, 'dev0')
+VERSION = (1, 0, 6)  # (1, 0, 7, 'dev0')
 __version__ = '.'.join(map(str, VERSION))
 
 setup(
@@ -17,7 +17,7 @@ setup(
     version=__version__,
     author='Michel Metran',
     author_email='michelmetran@gmail.com',
-    description='O presente repositório objetiva disponibilizar os parâmetros de qualidade em formato tabular, adequado para utilização em análises computacionais.',
+    description='O presente projeto objetiva disponibilizar os parâmetros de qualidade em formato adequado para utilização em análises computacionais',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/gaemapiracicaba/norma_dec_8468-76',
