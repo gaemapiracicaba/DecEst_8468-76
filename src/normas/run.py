a61e279b67f76c334b0197de05b6a9c4f01745d9
Michel Metran <michelmetran@gmail.com>
2022-02-04 02:36:56 -0300

    Ajustes no Pycharm

--- src/normas/run.py
+++ src/normas/run.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
-print(os.getcwd())
-
-
-print(this_filename)
-
-from decreto_estadual_8468 import *
-
-
-
+from normas import decreto_estadual_8468
 
 # Get Table
-df_8468, list_classes = get_8468_parameters()
+df_8468, list_classes = decreto_estadual_8468.get_parameters()
 
 # Filter Data by "Classe"
-df_8468, list_parametros = filter_by_classe(df_8468, classe='Classe 2')
+df_8468, list_parametros = decreto_estadual_8468.filter_by_classe(df_8468, classe='Classe 2')
 
 # Filter Data by "Parâmetros"
-dict_8468 = filter_by_parameters(df_8468, parametro='Oxigênio Dissolvido')
+dict_8468 = decreto_estadual_8468.filter_by_parameters(df_8468, parametro='Oxigênio Dissolvido')
 print(dict_8468)
 
 # Set Tipo
-set_type_desconformidade(dict_8468)
+decreto_estadual_8468.set_type_desconformidade(dict_8468)
